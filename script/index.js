8ca0c7017259b569721c0991e21a37ec725b6ecb
Oleh <lzerocool07@gmail.com>
2016-07-12 15:30:39 +0000

    add new file

---
+++ script/index.js
@@ -0,0 +1,19 @@
+document.getElementById('Company').onclick = function(){
+    document.getElementById('Company_1').style.display='block';
+    document.getElementById('News_1').style.display='none';
+    document.getElementById('Products_1').style.display ='none';
+    event.preventDefault();
+    
+}
+document.getElementById('Products').onclick = function(){
+    document.getElementById('Products_1').style.display ='block';
+    document.getElementById('Company_1').style.display='none';
+    document.getElementById('News_1').style.display='none';
+    event.preventDefault();
+}
+document.getElementById('News').onclick = function(){
+    document.getElementById('News_1').style.display='block';
+    document.getElementById('Company_1').style.display='none';
+    document.getElementById('News_1').style.display='none';
+    event.preventDefault();
+}
